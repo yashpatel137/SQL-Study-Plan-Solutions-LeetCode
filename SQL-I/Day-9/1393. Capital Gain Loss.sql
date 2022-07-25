2a56d9efb9eb5478db45c0c6fdaeebf921906829
Yash <86280730+yashpatel137@users.noreply.github.com>
2022-07-25 15:22:28 +0530

    Update 1393. Capital Gain Loss.sql

--- SQL-I/Day-9/1393. Capital Gain Loss.sql
+++ SQL-I/Day-9/1393. Capital Gain Loss.sql
@@ -1,3 +1,15 @@
 SELECT stock_name, (SUM(if(operation = 'Sell', price, 0))  -  SUM(if(operation = 'Buy', price, 0))) AS  capital_gain_loss 
 FROM Stocks
 GROUP BY stock_name;
+
+//Method 2
+
+SELECT stock_name,
+SUM(
+    CASE 
+        WHEN operation = 'Buy' THEN -price
+        ELSE price
+    END 
+) AS capital_gain_loss
+FROM Stocks
+GROUP By stock_name;
